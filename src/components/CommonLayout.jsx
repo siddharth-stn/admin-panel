dbc7c46e2193cc5d87f54fedf5d3560b5e0fce74
Siddharth Pande <sidarjunstn.sp@gmail.com>
2026-03-21 19:23:55 +0530

    adds the basic structure to the code

--- src/components/CommonLayout.jsx
+++ src/components/CommonLayout.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router";
 import "../Styles/styles.css";
+import SidePanel from "./common/SidePanel";
+import Header from "./common/Header";
 
 export default function CommonLayout() {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="website-wrapper w-full h-screen grid grid-cols-[minmax(250px,300px)_1fr]">
+      <section className="side-panel-wrapper col-start-1">
+        <SidePanel />
+      </section>
+      <section className="main-content-wrapper col-start-2">
+        <div className="header-wrapper">
+          <Header />
+        </div>
+        <div className="outlet-wrapper h-full">
+          <Outlet />
+        </div>
+      </section>
+    </div>
   );
 }
---
+++ src/components/common/SidePanel.jsx
@@ -0,0 +1,3 @@
+export default function SidePanel() {
+  return <div>SidePanel</div>;
+}
